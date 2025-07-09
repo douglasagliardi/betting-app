dab868986d11af2d168f2b97fe93954e712e27b2
Douglas Agliardi <douglas.agliardi@digitalchargingsolutions.com>
2025-07-09 10:27:44 +0200

    updated test sources with configuration and testcontainers

--- src/test/java/com/sportygroup/betting/IntegrationTests.java
+++ src/test/java/com/sportygroup/betting/IntegrationTests.java
@@ -1,16 +1,24 @@
 package com.sportygroup.betting;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.context.annotation.Import;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @AutoConfigureMockMvc
 @AutoConfigureWireMock(port = 0)
+@Import(TestcontainersConfiguration.class)
+@Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest(
     classes = BettingApplication.class,
     webEnvironment = WebEnvironment.RANDOM_PORT
 )
+@Target(ElementType.TYPE)
 @Testcontainers
 public @interface IntegrationTests { }
