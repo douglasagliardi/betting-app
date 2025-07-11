451da38ae54c934a757a0d24f6b5766081f1c6f1
Douglas Agliardi <douglas.agliardi@digitalchargingsolutions.com>
2025-07-11 10:15:50 +0200

    fixed coding style requirements

--- src/main/java/com/sportygroup/betting/usecase/InsufficientFundsException.java
+++ src/main/java/com/sportygroup/betting/usecase/InsufficientFundsException.java
@@ -2,7 +2,7 @@ package com.sportygroup.betting.usecase;
 
 public class InsufficientFundsException extends RuntimeException {
 
-  public InsufficientFundsException(long walletId, long amount) {
+  public InsufficientFundsException(final long walletId, final long amount) {
     super(String.format("unable to place bet with amount of %d using wallet %d", amount, walletId));
   }
 }
--- src/main/java/com/sportygroup/betting/usecase/NotEventWinnerFoundException.java
+++ src/main/java/com/sportygroup/betting/usecase/NotEventWinnerFoundException.java
@@ -2,7 +2,7 @@ package com.sportygroup.betting.usecase;
 
 public class NotEventWinnerFoundException extends RuntimeException {
 
-  public NotEventWinnerFoundException(long eventId) {
+  public NotEventWinnerFoundException(final long eventId) {
     super("winner for event with id " + eventId + " was not found");
   }
 }
